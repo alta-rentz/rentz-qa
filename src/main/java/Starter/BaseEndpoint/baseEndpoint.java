3862919be5316b183c6d324dcc4bc50e1b0a481e
Rahmanto <manto.otnamhar@gmail.com>
2021-12-20 06:40:05 +0700

    Update baseEndpoint.java

--- src/main/java/Starter/BaseEndpoint/baseEndpoint.java
+++ src/main/java/Starter/BaseEndpoint/baseEndpoint.java
@@ -4,4 +4,7 @@ public class baseEndpoint {
     public String BASE_URL = "https://rentz-id.site";
     public String REGISTER = BASE_URL + "/signup";
     public String LOGIN = BASE_URL + "/signin";
+    public String GetAllProduct = BASE_URL+"/products";
+    public String GetProductID = BASE_URL+"/products/";
+    public String GetProductSID = BASE_URL+"/products/subcategory/";
 }
